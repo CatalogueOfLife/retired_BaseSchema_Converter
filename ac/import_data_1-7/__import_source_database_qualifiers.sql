9c798acc118dd26ff55e035717c32a8f44f587f8
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2011-07-01 14:28:33 +0000

    BSC-24 Added data that should be spidered from metadatabase in the future but now has been imported from Excel files

---
+++ ac/import_data_1-7/__import_source_database_qualifiers.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 01, 2011 at 04:39 PM
+-- Server version: 5.5.9
+-- PHP Version: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `base_scheme_2011_19`
+--
+
+--
+-- Dumping data for table `__import_source_database_qualifiers`
+--
+
+INSERT INTO `__import_source_database_qualifiers` VALUES('AFD (Pulmonata)', 'Regional (Africa)', 52, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('AlgaeBase', 'Regional', 55, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('BIOS', 'Regional (Netherlands)', 90, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('CCW', 'Regional', 91, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('CIPA', 'Regional (Netherlands)', 97, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Species 2000 Common Names', 'Worldwide', 94, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('TIGR Reptiles', 'Regional', 99, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ETI WBD (Euphausiacea)', 'Regional (Africa)', 50, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('FishBase', 'Regional (Netherlands)', 63, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('FLOW', 'Worldwide', 52, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Glomeromycota', 'Regional (Netherlands)', 64, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Hexacorals', 'Regional (Netherlands)', 82, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ICTV_MSL', 'Regional', 98, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ILDIS', 'Regional (Africa)', 67, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('IOPI-GPC', 'Regional', 97, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ITIS', 'Regional (Netherlands)', 60, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('LepIndex', 'Worldwide', 75, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('MOST', 'Worldwide', 89, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Odonata', 'Worldwide', 90, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('OrthopteraSF', 'Regional', 79, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Parhost', 'Regional (Africa)', 87, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Phyllachorales', 'Regional (Africa)', 76, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WCSP', 'Worldwide', 66, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Rhytismatales', 'Worldwide', 77, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ScaleNet', 'Regional (Netherlands)', 75, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Scarabs', 'Regional', 88, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Species Fungorum', 'Regional (Netherlands)', 82, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('SpidCat', 'Regional (Netherlands)', 93, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('TicksBase', 'Regional (Netherlands)', 77, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Tineidae NHM', 'Regional (Netherlands)', 71, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('TITAN', 'Regional (Netherlands)', 78, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Trichomycetes', 'Worldwide', 52, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('UCD', 'Regional', 79, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('URMO', 'Worldwide', 66, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Xylariaceae', 'Regional', 58, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ZOBODAT (Vespoidea)', 'Regional (Netherlands)', 81, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Zygomycetes', 'Regional', 59, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WTaxa', 'Worldwide', 87, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('AnnonBase', 'Regional', 78, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('SysMyr', 'Regional (Netherlands)', 94, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ChiloBase', 'Regional', 59, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('LecyPages', 'Regional', 65, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Porifera', 'Worldwide', 58, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Conifer Database', 'Worldwide', 67, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('GloBIS (GART)', 'Worldwide', 54, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Rotifera', 'Regional', 72, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('RJB Geranium', 'Regional', 53, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('World Gracillariidae', 'Regional', 52, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('PhasmidaSF', 'Regional', 94, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('BlattodeaSF', 'Worldwide', 94, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('COOL', 'Worldwide', 51, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Nomen.eumycetozoa.com', 'Regional (Africa)', 74, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Psyllist', 'Regional (Africa)', 60, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('LDL Neuropterida', 'Regional (Netherlands)', 50, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('LHD', 'Regional (Africa)', 90, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Brachiopoda & Phoronida', 'Regional', 69, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Cumacea', 'Worldwide', 62, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Ophiuroidea', 'Regional (Netherlands)', 55, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Proseriata & Kalyptorhynchia', 'Regional (Netherlands)', 85, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('AphidSF', 'Regional (Netherlands)', 95, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('MantodeaSF', 'Regional (Netherlands)', 67, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('OlogamasidBase', 'Regional (Netherlands)', 75, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Solanaceae Source', 'Regional (Africa)', 69, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('PlecopteraSF', 'Regional (Africa)', 73, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Droseraceae Database', 'Regional (Africa)', 97, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ITIS Bees', 'Worldwide', 61, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Taxapad Ichneumonoidea', 'Regional (Netherlands)', 99, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('RhodacaridBase', 'Worldwide', 87, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('PhytoseiidBase', 'Regional (Africa)', 80, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('SalticidDB', 'Regional', 93, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('GCC', 'Regional (Africa)', 96, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Brassicaceae', 'Regional', 84, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('ELPT', 'Worldwide', 69, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('NZIB', 'Worldwide', 67, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('MBB', 'Regional (Netherlands)', 87, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('FWCrabs', 'Regional (Africa)', 83, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('TenuipalpidBase', 'Regional (Netherlands)', 97, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('LIAS', 'Regional (Africa)', 68, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('CunaxidBase', 'Regional (Netherlands)', 100, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Bryozoa', 'Worldwide', 83, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('SpmWeb', 'Worldwide', 52, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('3i Cicadellinae', 'Worldwide', 69, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('3i Typhlocybinae', 'Regional (Africa)', 62, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Nemertea', 'Worldwide', 84, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Bochusacea', 'Regional (Africa)', 91, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Brachypoda', 'Regional (Netherlands)', 84, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Mystacocarida', 'Regional', 78, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('EmbiopteraSF', 'Worldwide', 70, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Polychaeta', 'Worldwide', 90, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Remipedia', 'Regional (Netherlands)', 75, 1, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Tantulocarida', 'Regional (Africa)', 70, 3, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Thermosbaenacea', 'Regional (Netherlands)', 50, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Isopoda', 'Worldwide', 66, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Asteroidea', 'Regional (Netherlands)', 50, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('MOWD', 'Regional (Africa)', 60, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Saccharomycetes', 'Regional', 71, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Dothideomycetes', 'Regional (Africa)', 50, 5, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Oligochaeta', 'Worldwide', 55, 2, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('WoRMS Xenoturbellida', 'Regional', 92, 4, '2011-03-09 10:17:41');
+INSERT INTO `__import_source_database_qualifiers` VALUES('Systema Dipterorum', 'Regional (Netherlands)', 96, 5, '2011-03-09 10:17:41');
